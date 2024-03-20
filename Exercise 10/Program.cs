0a074ca37442a0b6b00f10ec982ab7b5cbc4ffbd
chris : ) <Christopher.Baker@s.capregboces.org>
2024-03-20 08:50:31 -0400

    “Ready for grading”.
    “Ready for grading”.

--- Exercise 10/Program.cs
+++ Exercise 10/Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         { 
             int MenuOpp = 0;
-            while (MenuOpp == 4)
+            int OptionsOpp = 0;
+            int loading = 100;
+            int loading_precent = 0;
+            Random rnd = new Random();
+            while (MenuOpp != 4)
             {
            
             //print menu
@@ -34,17 +38,71 @@
                 }
                 else if (MenuOpp == 2)
                 {
+                    if (loading <= 0)
+                    {
+                        loading = 100;
+                        loading_precent = 0;
+                    }
                     Console.WriteLine("Loading...............");
+                    while (loading > 0)
+                    {
+                        
+                        int tick1 = rnd.Next(1, 21);
+                      
+                        if (tick1 < 10)
+                        {
+                            int tick2 = rnd.Next(1, 11);
+                            if (tick2 >= 7)
+                            {
+                                loading--;
+                                loading_precent++;
+                                Console.WriteLine(loading_precent + "%");
+                            }
+
+                        }
+                        else if (tick1 == 20)
+                        {
+                            loading -= 5;
+                            loading_precent += 5;
+                            Console.WriteLine(loading_precent + "%");
+                        }
+
+
+                    }
                 }
                 else if (MenuOpp == 3)
                 {
-                    Console.WriteLine("Options are coming soon!!!!");
+                    while(OptionsOpp != 4)
+                    {
+                        //print Options
+                        Console.WriteLine("**************");
+                        Console.WriteLine("Options:");
+                        Console.WriteLine("1 - Coming soon");
+                        Console.WriteLine("2 - Coming soon");
+                        Console.WriteLine("3 - Coming soon?");
+                        Console.WriteLine("4 - back");
+                        Console.WriteLine("**************");
+                        Console.WriteLine();
+                        Console.WriteLine("Please Chosse a number from the options:");
+                        OptionsOpp = int.Parse(Console.ReadLine());
+                        if (OptionsOpp == 1 || OptionsOpp == 2 || OptionsOpp == 3)
+                        {
+                            Console.WriteLine("Coming soon......");
+                            MenuOpp = 0;
+                        }
+               
+                    }
+                if (OptionsOpp == 4)
+                    {
+                        OptionsOpp = 0;
+                    }
+
                 }
                 else if (MenuOpp > 4 || MenuOpp < 1)
                 {
                     Console.WriteLine("no");
                 }
-
+            
             }
 
         }
